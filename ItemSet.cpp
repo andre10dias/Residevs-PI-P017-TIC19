a8f84c4793f493425b0985eb74a45bff943f18f5
andre10dias <andre10dias@ymail.com>
2023-10-06 07:45:48 -0300

    Criando classe

---
+++ ItemSet.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class ItemSet
+{
+    private:
+        /* data */
+    public:
+        ItemSet(/* args */);
+        ~ItemSet();
+};
+
+ItemSet::ItemSet(/* args */)
+{
+}
+
+ItemSet::~ItemSet()
+{
+}
