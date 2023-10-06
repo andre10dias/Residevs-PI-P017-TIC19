da19488fb3cf632be9ae3096a66e5245ecac4f3f
andre10dias <andre10dias@ymail.com>
2023-10-06 20:20:38 -0300

    Etapa 1 - Individual

--- ItemSet.cpp
+++ ItemSet.cpp
@@ -8,12 +8,15 @@ class ItemSet
 {
     private:
         vector<string> listaItens;
+        bool itemCadastrado(string s);
 
     public:
         ItemSet(/* args */);
         ~ItemSet();
         vector<string> getListaItens();
         void setListaItens(vector<string> _listaItens);
+        void inserir(string s);
+        void excluir(string s);
 };
 
 ItemSet::ItemSet(/* args */)
@@ -32,4 +35,79 @@ vector<string> ItemSet::getListaItens()
 void ItemSet::setListaItens(vector<string> _listaItens)
 {
     listaItens = _listaItens;
+}
+
+bool ItemSet::itemCadastrado(string _s) {
+    for (string s : listaItens)
+    {
+        if (s == _s)
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+void ItemSet::inserir(string s)
+{
+    int tam = listaItens.size();
+
+    if (tam == 0 || !itemCadastrado(s))
+    {
+        listaItens.push_back(s);
+    }
+}
+
+void ItemSet::excluir(string s)
+{
+    int tam = listaItens.size();
+
+    if (tam >= 0 && itemCadastrado(s))
+    {
+        for (int i = 0; i < tam; i++)
+        {
+            if (listaItens[i] == s)
+            {
+                listaItens.erase(listaItens.begin() + i);
+                break;
+            }
+        }
+    }
+}
+
+int main(void) {
+    ItemSet itemSet;
+
+    cout << "\nInserindo itens" << endl;
+    itemSet.inserir("item1");
+    itemSet.inserir("item2");
+    itemSet.inserir("item3");
+    itemSet.inserir("item4");
+    itemSet.inserir("item5");
+
+    for (string s : itemSet.getListaItens())
+    {
+        cout << s << "\t";
+    }
+
+    cout << "\n\nTentando inserir item4 novamente" << endl;
+    itemSet.inserir("item4");
+
+    for (string s : itemSet.getListaItens())
+    {
+        cout << s << "\t";
+    }
+
+    cout << "\n\nExcluindo itens 3 e 5" << endl;
+    itemSet.excluir("item5");
+    itemSet.excluir("item3");
+
+    for (string s : itemSet.getListaItens())
+    {
+        cout << s << "\t";
+    }
+    
+    cout << endl << endl;
+    return 0;
 }
