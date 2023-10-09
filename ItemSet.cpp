a892d1428a7860cd80adf95851306d630b5caab4
leogmc <ifbaianoleo@gmail.com>
2023-10-09 02:53:39 -0300

    Adicionando sobrecarga de '=' e '*'

--- ItemSet.cpp
+++ ItemSet.cpp
@@ -6,29 +6,63 @@ using namespace std;
 
 class ItemSet
 {
-    private:
-        vector<string> listaItens;
-        bool itemCadastrado(string s);
+private:
+    vector<string> listaItens;
+    bool itemCadastrado(string s);
 
-    public:
-        ItemSet(/* args */);
-        ~ItemSet();
-        vector<string> getListaItens();
-        void setListaItens(vector<string> _listaItens);
-        void inserir(string s);
-        void excluir(string s);
-        ItemSet operator+(ItemSet c);
+public:
+    ItemSet();
+    ItemSet(const ItemSet &b);
+    ~ItemSet();
+    vector<string> getListaItens();
+    void setListaItens(vector<string> _listaItens);
+    void inserir(string s);
+    void excluir(string s);
+
+    ItemSet operator+(ItemSet c);
+    ItemSet &operator=(const ItemSet &b);
+    ItemSet operator*(ItemSet c);
+
+    
 };
 
-ItemSet::ItemSet(/* args */)
+ItemSet::ItemSet() {}
+
+// Construtor que recebe outro ItemSet como parâmetro
+ItemSet::ItemSet(const ItemSet &b)
 {
+    // Copiar os itens do conjunto 'other' para o novo conjunto
+    for (const std::string &item : b.listaItens)
+    {
+        this->inserir(item);
+    }
+}
+
+// Sobrecarga do operador de atribuição '='
+ItemSet &ItemSet::operator=(const ItemSet &b)
+{
+    if (this == &b)
+    {
+        return *this; // Evitar auto-atribuição
+    }
+
+    // Limpar os itens do conjunto atual
+    this->listaItens.clear();
+
+    // Copiar os itens do conjunto 'other' para o conjunto atual
+    for (const std::string &item : b.listaItens)
+    {
+        this->inserir(item);
+    }
+
+    return *this;
 }
 
 ItemSet::~ItemSet()
 {
 }
 
-vector<string> ItemSet::getListaItens() 
+vector<string> ItemSet::getListaItens()
 {
     return listaItens;
 }
@@ -38,7 +72,8 @@ void ItemSet::setListaItens(vector<string> _listaItens)
     listaItens = _listaItens;
 }
 
-bool ItemSet::itemCadastrado(string _s) {
+bool ItemSet::itemCadastrado(string _s)
+{
     for (string s : listaItens)
     {
         if (s == _s)
@@ -77,7 +112,8 @@ void ItemSet::excluir(string s)
     }
 }
 
-ItemSet ItemSet::operator+(ItemSet c) {
+ItemSet ItemSet::operator+(ItemSet c)
+{
     ItemSet a, b;
     bool inserir;
     a.setListaItens(listaItens);
@@ -100,11 +136,33 @@ ItemSet ItemSet::operator+(ItemSet c) {
             a.inserir(itemC);
         }
     }
-    
+
     return a;
 }
 
-int main(void) {
+ItemSet ItemSet::operator*(ItemSet c){
+    ItemSet b, interssecao;
+    vector<string> itensU;
+    
+    b.setListaItens(listaItens);
+
+    for (string itemB : listaItens)
+    {
+        for (string itemC : c.listaItens)
+        {
+            if (itemB == itemC)
+            itensU.push_back(itemC);
+        }
+    }
+
+    interssecao.setListaItens(itensU);
+
+    return interssecao;
+}
+
+
+int main(void)
+{
     ItemSet itemA, itemB, itemC;
 
     vector<string> listaItensB = {"1", "2", "3", "4", "5"};
@@ -113,8 +171,6 @@ int main(void) {
     itemB.setListaItens(listaItensB);
     itemC.setListaItens(listaItensC);
 
-    //'A' recebe todos os itens de 'B' e os itens de 'C' que não se repetem em 'B'.
-    itemA = itemB + itemC;
 
     cout << "\nB =\t";
     for (string s : itemB.getListaItens())
@@ -128,12 +184,30 @@ int main(void) {
         cout << s << "\t";
     }
 
+    itemA = itemB;
+    cout << "\n\nA = B =>\t";
+    for (string s : itemA.getListaItens())
+    {
+        cout << s << "\t";
+    }
+
+    //'A' recebe todos os itens de 'B' e os itens de 'C' que não se repetem em 'B'.
+    itemA = itemB + itemC;
     cout << "\n\nA = B + c =>\t";
     for (string s : itemA.getListaItens())
     {
         cout << s << "\t";
     }
 
-    cout << endl << endl;
+    //A recebe os itens de B que ocorrem também em C.
+    itemA = itemB * itemC;
+    cout << "\n\nA = B * C =>\t";
+    for (string s : itemA.getListaItens())
+    {
+        cout << s << "\t";
+    }
+
+    cout << endl
+         << endl;
     return 0;
 }
